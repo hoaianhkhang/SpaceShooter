a24ec9ee15c68e95669c055a3cad8c576b76d6eb
hoaianhkhang <46699312+hoaianhkhang@users.noreply.github.com>
2019-04-02 23:05:52 -0400

    Update GameMusic.js

--- Sound/Music/GameMusic.js
+++ Sound/Music/GameMusic.js
@@ -1,1 +1,16 @@
 
+import { Howl } from 'howler'
+import EpicSoundtrack from './EpicSoundtrack.mp3'
+import SynthSoundtrack from './SynthSoundtrack.mp3'
+import UpbeatSoundtrack from './UpbeatSoundtrack.mp3'
+
+var songs = new Howl({
+  src: ['./EpicSoundtrack.mp3', './SynthSoundtrack.mp3', './UpbeatSoundtrack.mp3'],
+  loop: true,
+  autoplay: true,
+  onend: function() {
+    console.log('Finished Playing.');
+  }
+});
+songs.play();
+// Credit Music: https://www.bensound.com
